29c90d9e820ecf1c7396e7adb0691ad77368f5dd
Leo Prada <leo.prada90@gmail.com>
2023-08-27 00:18:38 -0500

    8074366

---
+++ assets/CleanLayout.a46e77fd.js
@@ -0,0 +1,1 @@
+import{a3 as o,u as r,k as t,l as n}from"./index.4b59db85.js";const a={};function c(s,_){const e=r("router-view");return t(),n(e)}var f=o(a,[["render",c]]);export{f as default};
---
+++ assets/use-quasar.c623c1e6.js
@@ -0,0 +1,1 @@
+import{a,a0 as r}from"./index.4b59db85.js";function u(){return a(r)}export{u};
