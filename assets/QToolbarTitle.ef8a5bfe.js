9cc9593550e6fe0ccb94b6fdafb3439b05061809
Leo Prada <leo.prada90@gmail.com>
2024-06-24 16:55:13 -0500

    remove files

--- assets/QToolbarTitle.ef8a5bfe.js
+++
@@ -1,1 +0,0 @@
-import{c as s,h as t}from"./render.ae41ed0a.js";import{c as a,h as l}from"./index.4b59db85.js";var p=s({name:"QToolbarTitle",props:{shrink:Boolean},setup(o,{slots:e}){const r=a(()=>"q-toolbar__title ellipsis"+(o.shrink===!0?" col-shrink":""));return()=>l("div",{class:r.value},t(e.default))}});export{p as Q};
--- assets/use-quasar.c623c1e6.js
+++
@@ -1,1 +0,0 @@
-import{a,a0 as r}from"./index.4b59db85.js";function u(){return a(r)}export{u};
